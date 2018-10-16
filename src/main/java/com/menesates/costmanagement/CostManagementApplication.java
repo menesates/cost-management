5c0da5969b3e682a42e8df80b41dcbd5e254467e
Muhammed Enes Ates <atesmuhammedenes@gmail.com>
2018-10-16 21:23:14 +0300

    Proje Olusturuldu. Calistirildi ve localhost:8080/actuator/health ile kontrol edildi.

---
+++ src/main/java/com/menesates/costmanagement/CostManagementApplication.java
@@ -0,0 +1,11 @@
+package com.menesates.costmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostManagementApplication {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(CostManagementApplication.class);
+    }
+}
