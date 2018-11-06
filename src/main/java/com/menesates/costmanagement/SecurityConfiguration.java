524b9dba3e60a1cffab665f25d0a040435afb3ae
Muhammed Enes Ates <atesmuhammedenes@gmail.com>
2018-11-06 19:29:13 +0300

    thymeleaf devreye alındı. resources dizini altında templates dizini ve altında index ve login sayfları oluşturuldu. resources dizini altında public -> css dizi oluşturuldu ve login.css dosyası oluşturuldu. Security configuration ile login sayfası olarak tasarlanan sayfanın kullanımı sağlandı.

--- src/main/java/com/menesates/costmanagement/SecurityConfiguration.java
+++ src/main/java/com/menesates/costmanagement/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import javax.sql.DataSource;
@@ -21,6 +22,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.dataSource = dataSource;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**/favicon.ico",
+                "/css/**",
+                "js/**",
+                "/images/**",
+                "/webjars/**",
+                "/login.html").permitAll()
+                .antMatchers("/rest/**").access("hasRole('EDITOR')")
+                .antMatchers("/actuator/**").access("hasRole('ADMIN')")
+                .anyRequest().authenticated();
+
+        http.formLogin().loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                .failureUrl("/login.html?loginFailed=true");
+
+        // todo remember me yapılacak
+        //http.rememberMe().userDetailsService(userDetailsService);
+
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/menesates/costmanagement/web/CostManagementController.java
+++ src/main/java/com/menesates/costmanagement/web/CostManagementController.java
@@ -3,13 +3,22 @@ package com.menesates.costmanagement.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CostManagementController {
 
     @RequestMapping("/")
-    @ResponseBody
-    public String homePage(){
-        return "Welcome!";
+    public ModelAndView homePage(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/login.html")
+    public ModelAndView loginPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
     }
 }
